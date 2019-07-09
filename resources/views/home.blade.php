70b225125e84f2a2b0dbbec0237304e47c7acfdb
Raywire <ryanwire@outlook.com>
2019-07-09 20:31:43 +0300

    Designs the UI from Instagram

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,31 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img width="150px" src="https://instagram.fnbo2-1.fna.fbcdn.net/vp/331dc7d1f3d92334c5af55b03621f670/5DA14CC8/t51.2885-19/s320x320/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=instagram.fnbo2-1.fna.fbcdn.net" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>freecodecamp</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23K</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
             </div>
+            <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
+            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+            <div><a href="www.freecodecamp.org">www.freecodecamp.org</a></div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://instagram.fnbo2-1.fna.fbcdn.net/vp/f3f01aae5730ac3494fc0ae6baaf0e6e/5DBE2CA9/t51.2885-15/sh0.08/e35/c1.0.748.748a/s640x640/65142981_753889638347402_2885440414377677851_n.jpg?_nc_ht=instagram.fnbo2-1.fna.fbcdn.net" alt="" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fnbo2-1.fna.fbcdn.net/vp/9c4d950b37791984cb7823470ea2a6bb/5DBAF0A1/t51.2885-15/sh0.08/e35/c0.109.925.925a/s640x640/66170854_402202580395287_1871271487333362373_n.jpg?_nc_ht=instagram.fnbo2-1.fna.fbcdn.net" alt="" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fnbo2-1.fna.fbcdn.net/vp/6d5b1b6c896b915fce2236b0982305e5/5DA98D5F/t51.2885-15/sh0.08/e35/c1.0.747.747/s640x640/64772549_209695326687917_9055357715441137701_n.jpg?_nc_ht=instagram.fnbo2-1.fna.fbcdn.net" alt="" class="w-100">
         </div>
     </div>
 </div>
