80fe51a43ebcb8b78424b20f9265030fa33bfa84
Raywire <ryanwire@outlook.com>
2019-07-10 14:16:50 +0300

    Creates Profiles Controller, Models, Migrations and fetches the profile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,15 +7,15 @@
             <img width="150px" src="https://instagram.fnbo2-1.fna.fbcdn.net/vp/331dc7d1f3d92334c5af55b03621f670/5DA14CC8/t51.2885-19/s320x320/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=instagram.fnbo2-1.fna.fbcdn.net" class="rounded-circle">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>freecodecamp</h1></div>
+            <div><h1>{{ $user->username }}</h1></div>
             <div class="d-flex">
                 <div class="pr-5"><strong>153</strong> posts</div>
                 <div class="pr-5"><strong>23K</strong> followers</div>
                 <div class="pr-5"><strong>212</strong> following</div>
             </div>
-            <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
-            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
-            <div><a href="www.freecodecamp.org">www.freecodecamp.org</a></div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
+            <div><a href={{ $user->profile->url }}>{{ $user->profile->url }}</a></div>
         </div>
     </div>
     <div class="row pt-5">
