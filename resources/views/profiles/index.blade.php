79bdf611d2809eb3dd181881266b808c8969a1fa
Raywire <ryanwire@outlook.com>
2019-07-10 16:01:49 +0300

    Adds Posts to the Database & Many To Many Relationship

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -7,7 +7,10 @@
             <img width="150px" src="https://instagram.fnbo2-1.fna.fbcdn.net/vp/331dc7d1f3d92334c5af55b03621f670/5DA14CC8/t51.2885-19/s320x320/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=instagram.fnbo2-1.fna.fbcdn.net" class="rounded-circle">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>{{ $user->username }}</h1></div>
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user->username }}</h1>
+                <a href="#">Add new post</a>
+            </div>
             <div class="d-flex">
                 <div class="pr-5"><strong>153</strong> posts</div>
                 <div class="pr-5"><strong>23K</strong> followers</div>
