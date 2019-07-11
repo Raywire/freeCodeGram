a894590d3a767da3b134fad422a47039d9ed29e5
Raywire <ryanwire@outlook.com>
2019-07-11 11:16:52 +0300

    Enables editing the Profile

--- app/Profile.php
+++ app/Profile.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+    protected $guarded = []; // Disable mass assignment protection
+
     public function user()
     {
         return $this->belongsTo(User::class);
