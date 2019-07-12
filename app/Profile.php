aa582cf5d442baa10b2a585106980c5d47dc4e92
Raywire <ryanwire@outlook.com>
2019-07-12 10:51:39 +0300

    Adds a default Profile Image

--- app/Profile.php
+++ app/Profile.php
@@ -8,6 +8,12 @@ class Profile extends Model
 {
     protected $guarded = []; // Disable mass assignment protection
 
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : '/profile/bAIuFVoROUhXVvlulBVy8w4qNXj13FxhfqkIAdVh.png';
+        return '/storage/' . $imagePath;
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
