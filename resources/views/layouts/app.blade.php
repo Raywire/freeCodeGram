81d3ad8a5f6d39b9f7784bc3d251c2bafb5ddb5b
Raywire <ryanwire@outlook.com>
2019-07-13 11:52:27 +0300

    Adds link to my profile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,6 +62,7 @@
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
+                                    <a class="dropdown-item" href="/profile/{{ Auth::user()->id }}">My Profile</a>
                                 </div>
                             </li>
                         @endguest
